9e2409cde6b8ae42cdcabee63aa70e4d2f8d65f5
TheReverend403 <TheReverend403@cookieslap.net>
2013-08-03 14:10:04 +0100

    Some final tweaks for the day.

--- src/main/java/uk/co/revthefox/foxbot/FoxBot.java
+++ src/main/java/uk/co/revthefox/foxbot/FoxBot.java
@@ -51,8 +51,9 @@ public class FoxBot
         bot.setAutoNickChange(config.getAutoNickChange());
         bot.setAutoReconnect(config.getAutoReconnect());
         bot.setMessageDelay(config.getMessageDelay());
+        bot.setVersion("FoxBot - A Java IRC bot written by TheReverend403");
         bot.setName(config.getBotNick());
-        bot.setLogin(config.getBotRealName());
+        bot.setLogin(config.getBotIdent());
     }
 
     private void connectToServer()
