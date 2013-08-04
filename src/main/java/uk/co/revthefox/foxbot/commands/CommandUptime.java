cbdb49481f1642d92f6bf952a62e339c414c52bc
TheReverend403 <TheReverend403@cookieslap.net>
2013-08-04 13:35:17 +0100

    Trying to improve uptime command

--- src/main/java/uk/co/revthefox/foxbot/commands/CommandUptime.java
+++ src/main/java/uk/co/revthefox/foxbot/commands/CommandUptime.java
@@ -9,6 +9,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.text.DecimalFormat;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class CommandUptime extends Command
 {
@@ -28,9 +29,13 @@ public class CommandUptime extends Command
             try
             {
                 String uptime = new Scanner(new FileInputStream("/proc/uptime")).next();
-                Double unixTime = Double.valueOf(uptime) / 60 / 60 / 24;
-                channel.sendMessage(String.format("%sSystem uptime: %s%s days", Colors.GREEN, Colors.NORMAL,
-                        roundTwoDecimals(unixTime)));
+
+                int seconds = Integer.valueOf(uptime);
+                int day = (int) TimeUnit.SECONDS.toDays(seconds);
+                long hours = TimeUnit.SECONDS.toHours(seconds) - (day *24);
+                long minute = TimeUnit.SECONDS.toMinutes(seconds) - (TimeUnit.SECONDS.toHours(seconds)* 60);
+                long second = TimeUnit.SECONDS.toSeconds(seconds) - (TimeUnit.SECONDS.toMinutes(seconds) *60);
+                channel.sendMessage(String.format("%sSystem uptime: %s%s days %s hours %s minutes %s seconds", Colors.GREEN, Colors.NORMAL, day, hours, minute, second));
                 return;
             }
             catch (FileNotFoundException ex)
