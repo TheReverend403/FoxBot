000978f0fd50418a376383c3baf01c0a22d20167
Zack <Zack@Custom.home>
2013-08-07 05:40:41 -0400

    Here you go reb, you can ass faster url scanning :3

--- src/main/java/uk/co/revthefox/foxbot/Utils.java
+++ src/main/java/uk/co/revthefox/foxbot/Utils.java
@@ -1,18 +1,24 @@
 package uk.co.revthefox.foxbot;
 
 import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.AsyncHttpClientConfig;
 import com.ning.http.client.Response;
 import org.pircbotx.Colors;
 import org.pircbotx.User;
 
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Utils
 {
+
     private FoxBot foxbot;
 
     public Utils(FoxBot foxbot)
@@ -22,58 +28,43 @@ public class Utils
 
     public String parseChatUrl(String stringToParse, User sender)
     {
-        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
-        Future<Response> future;
-        Response response;
-
         try
         {
-            future = asyncHttpClient.prepareGet(stringToParse).setFollowRedirects(true).execute();
-            response = future.get();
-            if (!response.getStatusText().contains("OK") && !response.getStatusText().contains("Moved Permanently"))
+            AsyncHttpClientConfig cf = new AsyncHttpClientConfig.Builder().setUserAgent("Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36").build();
+            AsyncHttpClient client = new AsyncHttpClient(cf);
+            Future<Response> future = client.prepareGet(stringToParse).setFollowRedirects(true).execute();
+            Response response = future.get();
+            String output = response.getResponseBody("UTF-8");
+            URLConnection conn = new URL(stringToParse).openConnection();
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36");
+            String size = (conn.getContentLengthLong() / 1024) + "kb";
+            String content_type = conn.getContentType().contains(";") ? conn.getContentType().split(";")[0] : conn.getContentType();
+            
+            if (response.getStatusCode() != 200 && response.getStatusCode() != 302 && response.getStatusCode() != 301)
             {
-                return String.format("(%s's URL) %sError: %s%s %s ", sender.getNick(), Colors.RED, Colors.NORMAL,response.getStatusCode(), response.getStatusText());
+                return String.format("(%s's URL) %sError: %s%s %s ", sender.getNick(), Colors.RED, Colors.NORMAL, response.getStatusCode(), response.getStatusText());
             }
-        }
-        catch (IOException ex)
+            if (!content_type.contains("html"))
+            {
+                return "(" + sender.getNick() + "'s URL)" + Colors.GREEN + "Content Type: " + Colors.NORMAL + content_type + Colors.GREEN + " Size:" + Colors.NORMAL + (conn.getContentLengthLong() / 1024) + "kb";
+            }
+            
+            Pattern p = Pattern.compile("<title>.+</title>");
+            Matcher m;
+            String title = "Error?";
+            for (String line : output.split("\n"))
+            {
+                m = p.matcher(line);
+                if (m.find())
+                {
+                    title = line.split("<title>")[1].split("</title>")[0];
+                }
+            }
+            return String.format("(%s's URL) " + Colors.GREEN + "Title: " + Colors.NORMAL + "%s " + Colors.GREEN + "Content type: " + Colors.NORMAL + "%s " + Colors.GREEN + "Size: " + Colors.NORMAL + "%s", sender.getNick(), title, content_type, size);
+        } catch (Exception ex)
         {
-            ex.printStackTrace();
-            return "";
+            Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
         }
-        catch (InterruptedException ex)
-        {
-            ex.printStackTrace();
-            return "";
-        }
-        catch (ExecutionException ex)
-        {
-            System.out.println("That last URL appeared to be invalid...");
-            return "";
-        }
-
-        try
-        {
-            stringToParse = response.getResponseBody();
-        }
-        catch (IOException ex)
-        {
-            ex.printStackTrace();
-        }
-
-        Pattern titlePattern = Pattern.compile(".*?<title.*?>(.*)</title>.*?",
-                Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
-        Matcher titleMatcher = titlePattern.matcher(stringToParse);
-        while (titleMatcher.find())
-        {
-            stringToParse = titleMatcher.group(1);
-        }
-
-        if (stringToParse.length() > 100)
-        {
-            return String.format("(%s's URL) %sContent too big. This would have caused me to flood out.", sender.getNick(), Colors.RED);
-        }
-        return String.format("(%s's URL) %sTitle: %s%s %sContent type: %s%s", sender.getNick(), Colors.GREEN,
-                Colors.NORMAL, stringToParse.replace("&#039;", "'"), Colors.GREEN, Colors.NORMAL, response.getContentType().replaceAll("(;.*)", ""));
+        return "null";
     }
-
 }
