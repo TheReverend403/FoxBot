98beead69d9741256d01d533b1fe1605d2d41b90
zack6849 <zack6849@laptop>
2013-08-17 09:33:49 -0400

    i feexed et faget.

--- src/main/java/uk/co/revthefox/foxbot/commands/CommandExec.java
+++ src/main/java/uk/co/revthefox/foxbot/commands/CommandExec.java
@@ -1,13 +1,28 @@
 package uk.co.revthefox.foxbot.commands;
 
+import bsh.EvalError;
+import bsh.Interpreter;
+import bsh.UtilEvalError;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.lang3.StringUtils;
 import org.pircbotx.Channel;
 import org.pircbotx.User;
 import uk.co.revthefox.foxbot.FoxBot;
 
 public class CommandExec extends Command
 {
-    private FoxBot foxbot;
-
+    private static FoxBot foxbot;
+    private static Interpreter interpreter;
+    static{
+        try {
+            interpreter = new Interpreter();
+            interpreter.getNameSpace().doSuperImport();
+            interpreter.setStrictJava(false);
+        } catch (Exception ex) {
+            Logger.getLogger(CommandExec.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     public CommandExec(FoxBot foxbot)
     {
         super("exec", "command.exec");
@@ -17,6 +32,16 @@ public class CommandExec extends Command
     @Override
     public void execute(User sender, Channel channel, String[] args)
     {
-
+        try {
+            interpreter.set("sender", sender);
+            interpreter.set("channel", channel);            
+            interpreter.set("bot", foxbot.getBot());
+            interpreter.set("foxbot", foxbot);
+            interpreter.eval(StringUtils.join(args, " ").trim());
+            
+        } catch (EvalError ex) {
+            foxbot.getBot().sendMessage(channel, ex.getLocalizedMessage());
+            Logger.getLogger(CommandExec.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
