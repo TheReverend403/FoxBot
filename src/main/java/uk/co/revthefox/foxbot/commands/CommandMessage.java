42c6a596db9fde5b393191e640aec007c6afa8a0
TheReverend403 <TheReverend403@cookieslap.net>
2013-09-06 14:00:20 +0100

    Add PM command

---
+++ src/main/java/uk/co/revthefox/foxbot/commands/CommandMessage.java
@@ -0,0 +1,45 @@
+package uk.co.revthefox.foxbot.commands;
+
+import org.pircbotx.PircBotX;
+import org.pircbotx.User;
+import org.pircbotx.hooks.events.MessageEvent;
+import uk.co.revthefox.foxbot.FoxBot;
+
+public class CommandMessage extends Command
+{
+    private FoxBot foxbot;
+
+    public CommandMessage(FoxBot foxbot)
+    {
+        super("message", "command.message", "pm");
+        this.foxbot = foxbot;
+    }
+
+    @Override
+    public void execute(MessageEvent event, String[] args)
+    {
+        User sender = event.getUser();
+        PircBotX bot = foxbot.getBot();
+
+        if (args.length > 1)
+        {
+            User target = bot.getUser(args[0]);
+
+            if (target != null)
+            {
+                StringBuilder message = new StringBuilder(args[1]);
+
+                for (int arg = 2; arg < args.length; arg++)
+                {
+                    message.append(" ").append(args[arg]);
+                }
+
+                bot.sendMessage(target, message.toString());
+                return;
+            }
+            bot.sendNotice(sender, "That user does not exist!");
+            return;
+        }
+        bot.sendNotice(sender, String.format("Wrong number of args! use %smessage <user> <message>", foxbot.getConfig().getCommandPrefix()));
+    }
+}
