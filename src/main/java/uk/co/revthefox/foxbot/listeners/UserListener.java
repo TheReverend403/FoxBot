4f99c9266fa004f44470b94c8c8078dd4ee8c084
TheReverend403 <TheReverend403@cookieslap.net>
2013-09-07 18:46:20 +0100

    Moar fix

--- src/main/java/uk/co/revthefox/foxbot/listeners/UserListener.java
+++ src/main/java/uk/co/revthefox/foxbot/listeners/UserListener.java
@@ -47,7 +47,7 @@ public class UserListener extends ListenerAdapter<FoxBot>
         {
             for (Channel channel : foxbot.getChannels())
             {
-                if (!channel.getOps().contains(foxbot.getUser(foxbot.getNick())) && channel.getUsers().contains(user))
+                if (channel.getUsers().contains(user) && !channel.getOps().contains(foxbot.getUser(foxbot.getNick())))
                 {
                     foxbot.partChannel(channel, String.format("'%s' is on my protected nick list. I am not able to kick '%s', so I am leaving this channel as a security measure.", newNick, newNick));
                     continue;
