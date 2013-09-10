f0c37b113e3d36513f082bb6e708e740e5a0e271
TheReverend403 <TheReverend403@cookieslap.net>
2013-09-10 19:17:58 +0100

    Er....maybe I should run that in a new thread...

--- src/main/java/uk/co/revthefox/foxbot/commands/CommandMute.java
+++ src/main/java/uk/co/revthefox/foxbot/commands/CommandMute.java
@@ -20,42 +20,48 @@ public class CommandMute extends Command
     @Override
     public void execute(final MessageEvent event, final String[] args)
     {
-        User sender = event.getUser();
-        Channel channel = event.getChannel();
-        
-
-        if (args.length == 1)
+        new Thread(new Runnable()
         {
-            User target = foxbot.getUser(args[0]);
-            
-            if (!channel.getUsers().contains(target))
+            @Override
+            public void run()
             {
-                foxbot.sendNotice(sender, "That user is not in this channel!");
-                return;
-            }
+                User sender = event.getUser();
+                Channel channel = event.getChannel();
 
-            // Delay the mute to prevent whois throttling due to the permission check on both users
-            try
-            {
-                Thread.sleep(foxbot.getConfig().getKickDelay());
-            }
-            catch (InterruptedException ex)
-            {
-                ex.printStackTrace();
-            }
+                if (args.length == 1)
+                {
+                    User target = foxbot.getUser(args[0]);
 
-            if (foxbot.getPermissionManager().userHasQuietPermission(target, "protection.mute") || args[0].equals(foxbot.getNick()))
-            {
-                foxbot.sendNotice(sender, "You cannot mute that user!");
-                return;
+                    if (!channel.getUsers().contains(target))
+                    {
+                        foxbot.sendNotice(sender, "That user is not in this channel!");
+                        return;
+                    }
+
+                    // Delay the mute to prevent whois throttling due to the permission check on both users
+                    try
+                    {
+                        Thread.sleep(foxbot.getConfig().getKickDelay());
+                    }
+                    catch (InterruptedException ex)
+                    {
+                        ex.printStackTrace();
+                    }
+
+                    if (foxbot.getPermissionManager().userHasQuietPermission(target, "protection.mute") || args[0].equals(foxbot.getNick()))
+                    {
+                        foxbot.sendNotice(sender, "You cannot mute that user!");
+                        return;
+                    }
+
+                    String hostmask = "*" + target.getHostmask();
+
+                    foxbot.setMode(channel, "+q ", hostmask);
+                    foxbot.getUtils().scheduleModeRemove(channel, hostmask, "q", foxbot.getConfig().getUnbanTimer());
+                    return;
+                }
+                foxbot.sendNotice(sender, String.format("Wrong number of args! Use %smute <user>", foxbot.getConfig().getCommandPrefix()));
             }
-            
-            String hostmask = "*" + target.getHostmask();
-            
-            foxbot.setMode(channel, "+q ", hostmask);
-            foxbot.getUtils().scheduleModeRemove(channel, hostmask, "q", foxbot.getConfig().getUnbanTimer());
-            return;
-        }
-        foxbot.sendNotice(sender, String.format("Wrong number of args! Use %smute <user>", foxbot.getConfig().getCommandPrefix()));
+        }).start();
     }
 }
