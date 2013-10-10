6aad6cc6cec5ef268ea28e0f3e05d4faad6dfb50
TheReverend403 <thereverend403@revthefox.co.uk>
2013-10-10 12:48:19 +0100

    Log more pls.

--- src/main/java/co/foxdev/foxbot/listeners/UserListener.java
+++ src/main/java/co/foxdev/foxbot/listeners/UserListener.java
@@ -50,6 +50,8 @@ public class UserListener extends ListenerAdapter<FoxBot>
     @Override
     public void onConnect(ConnectEvent<FoxBot> event)
     {
+        BotLogger.log(LogLevel.INFO, String.format("CONNECT: Trying address %s", foxbot.getConfig().getServerAddress()));
+
         for (String line : foxbot.getServerInfo().getMotd().split("\n"))
         {
             BotLogger.log(LogLevel.INFO, String.format("CONNECT: %s", line));
