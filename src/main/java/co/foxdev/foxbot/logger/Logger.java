f5267c98cd8af02f89ba447bc1263219e0bca11c
TheReverend403 <thereverend403@revthefox.co.uk>
2013-10-10 12:08:26 +0100

    pls enum

--- src/main/java/co/foxdev/foxbot/logger/Logger.java
+++ src/main/java/co/foxdev/foxbot/logger/Logger.java
@@ -7,6 +7,6 @@ public class Logger
 {
     public static void log(Level level, String message)
     {
-        System.out.println(String.format("%s %s %s", new SimpleDateFormat("[yyyy-MM-dd - HH:mm:ss]").format(Calendar.getInstance().getTimeInMillis()), level.toString(), message));
+        System.out.println(String.format("%s [%s] %s", new SimpleDateFormat("[HH:mm:ss]").format(Calendar.getInstance().getTimeInMillis()), level, message));
     }
 }
