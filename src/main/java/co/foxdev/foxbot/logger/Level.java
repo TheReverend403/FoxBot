a7ee470fc51f1e48c2724cfb1975e2831d3885d7
TheReverend403 <thereverend403@revthefox.co.uk>
2013-10-10 11:55:49 +0100

    Testing logger

---
+++ src/main/java/co/foxdev/foxbot/logger/Level.java
@@ -0,0 +1,14 @@
+package co.foxdev.foxbot.logger;
+
+public enum Level
+{
+    INFO("[INFO]"),
+    WARNING("[WARNING]"),
+    SEVERE("[SEVERE]");
+
+    private Level(final String message) {
+        this.message = message;
+    }
+
+    private final String message;
+}
