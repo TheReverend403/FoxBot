f489caf6a0b3ac8755a71b4937ea62b6132f3ddc
Lee Watson <rev@revthefox.co.uk>
2013-11-23 17:00:47 +0000

    Fixed NPE and colourising

--- src/main/java/co/foxdev/foxbot/commands/CommandGeoip.java
+++ src/main/java/co/foxdev/foxbot/commands/CommandGeoip.java
@@ -27,8 +27,7 @@ public class CommandGeoip extends Command
             String country = foxbot.getLookupService().getLocation(ip).countryName;
             String city = foxbot.getLookupService().getLocation(ip).city;
 
-            channel.sendMessage(String.format("(%s) &2GeoIP info for %s:&r %s%s", foxbot.getUtils().munge(sender.getNick()), ip, city == null ? "" : city, country == null ? "" : city == null ? country : ", " + country));
-            foxbot.getLookupService().close();
+            channel.sendMessage(foxbot.getUtils().colourise(String.format("(%s) &2GeoIP info for %s:&r %s%s", foxbot.getUtils().munge(sender.getNick()), ip, city == null ? "" : city, country == null ? "" : city == null ? country : ", " + country)));
             return;
         }
         foxbot.sendNotice(sender, String.format("Wrong number of args! Use %sgeoip <host|user>", foxbot.getConfig().getCommandPrefix()));
