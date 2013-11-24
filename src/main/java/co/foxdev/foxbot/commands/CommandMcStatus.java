1ba4baad179db92d0daf4f51ae520dfad06b7f05
Lee Watson <rev@revthefox.co.uk>
2013-11-24 18:49:19 +0000

    Added mcstatus command

---
+++ src/main/java/co/foxdev/foxbot/commands/CommandMcStatus.java
@@ -0,0 +1,60 @@
+package co.foxdev.foxbot.commands;
+
+import co.foxdev.foxbot.FoxBot;
+import org.pircbotx.Channel;
+import org.pircbotx.Colors;
+import org.pircbotx.User;
+import org.pircbotx.hooks.events.MessageEvent;
+
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class CommandMcStatus extends Command
+{
+    private final FoxBot foxbot;
+    private String online = Colors.DARK_GREEN + "✔";
+    private String offline = Colors.RED + "✘";
+
+    public CommandMcStatus(FoxBot foxbot)
+    {
+        super("mcstatus", "command.mcstatus", "mcs");
+        this.foxbot = foxbot;
+    }
+
+    @Override
+    public void execute(final MessageEvent event, final String[] args)
+    {
+        User sender = event.getUser();
+        Channel channel = event.getChannel();
+
+        if (args.length == 0)
+        {
+            StringBuilder statusString = new StringBuilder("(%s) ");
+
+            String[] urls = new String[]{
+                    "minecraft.net",
+                    "login.minecraft.net",
+                    "session.minecraft.net",
+                    "skins.minecraft.net",
+                    "realms.minecraft.net"
+            };
+
+            for (String url : urls)
+            {
+                try
+                {
+                    Socket socket = new Socket(InetAddress.getByName(url), 80);
+                    statusString.append("| ").append(url).append(" ").append(online).append(" ");
+                    socket.close();
+                }
+                catch (Exception ex)
+                {
+                    statusString.append("| ").append(url).append(" ").append(offline).append(" ");
+                }
+            }
+            statusString.append("|");
+            return;
+        }
+        foxbot.sendNotice(sender, String.format("Wrong number of args! Use %smcstatus", foxbot.getConfig().getCommandPrefix()));
+    }
+}
