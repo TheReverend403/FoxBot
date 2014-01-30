b2cfeaef6d69ca2bb94f33c26f34f292384c2e4d
Lee Watson <rev@revthefox.co.uk>
2014-01-30 17:24:36 +0000

    Oops

--- src/main/java/co/foxdev/foxbot/plugin/PluginManager.java
+++ src/main/java/co/foxdev/foxbot/plugin/PluginManager.java
@@ -125,7 +125,7 @@ public class PluginManager
             {
                 String[] lines = LINES_SPLIT.split(message.toString());
 
-                for (int i = 0; i <= 3; i++)
+                for (int i = 0; i <= 2; i++)
                 {
                     foxbot.getChannel(channel).sendMessage(foxbot.getConfig().getCommandPrefix() + command + ": " + lines[i]);
                 }
