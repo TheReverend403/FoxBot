44dd8b3c5c860b875c0b782cbd79ac21a956a88b
Lee Watson <rev@revthefox.co.uk>
2014-01-30 01:10:35 +0000

    k

--- src/main/java/co/foxdev/foxbot/utils/Utils.java
+++ src/main/java/co/foxdev/foxbot/utils/Utils.java
@@ -66,7 +66,7 @@ public class Utils
 		        BotLogger.log(Level.INFO, header + " " + response.headers().get(header));
 	        }
 
-            String size = response.header("Content-Length") + "kb";
+            String size = response.header("Content-Length") == null ? "Unknown" : (Integer.parseInt(response.header("Content-Length")) / 1024) + "kb";
             String contentType = response.contentType().contains(";") ? response.contentType().split(";")[0] : response.contentType();
 
             if (response.statusCode() != 200 && response.statusCode() != 302 && response.statusCode() != 301)
