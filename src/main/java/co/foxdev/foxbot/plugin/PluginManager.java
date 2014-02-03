dcf2f7dbbd351bd277ed0342294f26cd6d86cfcc
Lee Watson <rev@revthefox.co.uk>
2014-02-03 11:45:17 +0000

    Oh, fuck off.

--- src/main/java/co/foxdev/foxbot/plugin/PluginManager.java
+++ src/main/java/co/foxdev/foxbot/plugin/PluginManager.java
@@ -59,54 +59,7 @@ public class PluginManager
         String commandName = split[0].toLowerCase();
         Command command = commandMap.get(commandName);
 
-	    List<Command> matches = new ArrayList<>();
-
-	    if (runCustomCommand(event.getChannel().getName(), commandName))
-	    {
-		    return true;
-	    }
-
-	    for (Command cmd : commandMap.values())
-	    {
-		    if ((commandName + ".*").matches(cmd.getName()))
-		    {
-			    matches.add(cmd);
-		    }
-	    }
-
-	    if (matches.size() > 1)
-	    {
-		    StringBuilder sb = new StringBuilder();
-
-		    for (int i = 0; i < sb.length(); i++)
-		    {
-			    sb.append(matches.get(i)).append(", ");
-		    }
-
-		    String commandMatches = sb.toString();
-
-		    foxbot.sendNotice(sender, String.format("'%s' matches %s commands: %s", commandName, matches.size(), commandMatches.substring(0, commandMatches.lastIndexOf(","))));
-		    return false;
-	    }
-
-	    if (matches.size() == 0)
-	    {
-		    return false;
-	    }
-
-	    if (!commandMap.containsKey(commandName))
-	    {
-		    StringBuilder commandBuilder = new StringBuilder(matches.get(0).getName());
-
-		    for (int i = 1; i < commandBuilder.length(); i++)
-		    {
-			    commandBuilder.append(split[i]).append(" ");
-		    }
-
-		    return dispatchCommand(event, commandBuilder.toString());
-	    }
-
-	    if (command == null)
+	    if (!runCustomCommand(event.getChannel().getName(), commandName) && command == null)
 	    {
 		    return false;
 	    }
