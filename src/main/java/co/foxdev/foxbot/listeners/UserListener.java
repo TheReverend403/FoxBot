13d9ab5bda26a47cce54d4c3078f56d7103af891
TheReverend403 <rev@revthefox.co.uk>
2014-03-14 00:07:39 +0000

    More reliable way of testing if a user can be kicked.

--- src/main/java/co/foxdev/foxbot/listeners/UserListener.java
+++ src/main/java/co/foxdev/foxbot/listeners/UserListener.java
@@ -119,17 +119,18 @@ public class UserListener extends ListenerAdapter<FoxBot>
 
         if (foxbot.getPermissionManager().isNickProtected(nick))
         {
-            if (!channel.isOp(foxbot.getUserBot()))
-            {
-                foxbot.partChannel(channel, String.format("'%s' is on my protected nick list. I am not able to kick '%s', so I am leaving this channel as a security measure.", nick, nick));
-                return;
-            }
-
             long kickTime = System.currentTimeMillis();
 
             foxbot.kick(channel, user, String.format("The nick '%s' is protected. Either connect with the associated hostmask or do not use that nick.", nick));
-            foxbot.getDatabase().addKick(channel, user, String.format("The nick '%s' is protected. Either connect with the associated hostmask or do not use that nick.", nick), foxbot.getUserBot(), kickTime);
-            return;
+
+	        if (!channel.getUsers().contains(user))
+	        {
+		        foxbot.getDatabase().addKick(channel, user, String.format("The nick '%s' is protected. Either connect with the associated hostmask or do not use that nick.", nick), foxbot.getUserBot(), kickTime);
+		        return;
+	        }
+
+	        foxbot.partChannel(channel, String.format("'%s' is on my protected nick list. I am not able to kick '%s', so I am leaving this channel as a security measure.", nick, nick));
+	        return;
         }
 
         if (!foxbot.getConfig().getGreetingChannels().isEmpty() && !foxbot.getPermissionManager().userHasQuietPermission(user, "greetings.ignore") && foxbot.getConfig().getGreetingChannels().contains(channel.getName()))
