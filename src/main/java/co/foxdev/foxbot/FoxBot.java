e19631f5837e91c62b57585f03dd55f911d17414
Lee Watson <rev@revthefox.co.uk>
2014-05-10 01:20:18 +0100

    In the event that CAP isn't enabled, enable it.

--- src/main/java/co/foxdev/foxbot/FoxBot.java
+++ src/main/java/co/foxdev/foxbot/FoxBot.java
@@ -36,6 +36,7 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.logging.Level;
 
 /**
